5c9940d0cd1a05055008978a14384be59fc0c9b6
faraznb <alifarazzain@gmail.com>
2021-12-10 16:15:39 +0500

    Custom Layout Dialog in progress

--- MyDialogBox/src/main/java/com/example/mydialogbox/MyDialogBox.kt
+++ MyDialogBox/src/main/java/com/example/mydialogbox/MyDialogBox.kt
@@ -5,14 +5,36 @@ import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.View
+import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintSet
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 object MyDialogBox {
 
-    fun toastMessage(context: Context, message: String) {
+    fun mToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun mDialogBox(context: Context,
+                   title: String,
+                   message: String,
+                   positiveButtonName: String,
+                   NegativeButtonName: String): MaterialAlertDialogBuilder {
+        val dialog = MaterialAlertDialogBuilder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(positiveButtonName) { p0, p1 ->
+                p0.dismiss()
+            }
+            .setNegativeButton(NegativeButtonName) { p0, p1 ->
+                p0.dismiss()
+            }
+        dialog.show()
+
+        return dialog
     }
 
     fun mDialogBox(context: Context,
@@ -21,7 +43,8 @@ object MyDialogBox {
                    positiveButtonName: String,
                    NegativeButtonName: String,
                    NeutralButtonName: String,
-                   onPositiveButtonToastMsg: String) : MaterialAlertDialogBuilder {
+                   onPositiveButtonToastMsg: String,
+                   onNegativeButtonMsg: String) : MaterialAlertDialogBuilder {
        val dialog =  MaterialAlertDialogBuilder(context)
             .setTitle(title)
             .setMessage(message)
@@ -30,7 +53,7 @@ object MyDialogBox {
                 p0.dismiss()
             }
            .setNegativeButton(NegativeButtonName) { p0, p1 ->
-               Toast.makeText(context, "Ok Enjoy", Toast.LENGTH_SHORT).show()
+               Toast.makeText(context, onNegativeButtonMsg, Toast.LENGTH_SHORT).show()
                p0.dismiss()
            }
            .setNeutralButton(NeutralButtonName) { p0, p1 ->
@@ -43,25 +66,60 @@ object MyDialogBox {
     }
 
 
-
     fun mDialogBox(context: Context,
                    title: String,
                    message: String,
                    positiveButtonName: String,
-                   NegativeButtonName: String): MaterialAlertDialogBuilder {
-        val dialog = MaterialAlertDialogBuilder(context)
-                    .setTitle(title)
-                    .setMessage(message)
-                    .setPositiveButton(positiveButtonName) { p0, p1 ->
-                        Toast.makeText(context, "You Click Yes Button", Toast.LENGTH_SHORT).show()
-                        p0.dismiss()
-                    }
-                    .setNegativeButton(NegativeButtonName) { p0, p1 ->
-                        Toast.makeText(context, "Ok Enjoy", Toast.LENGTH_SHORT).show()
-                        p0.dismiss()
-                    }
+                   NegativeButtonName: String,
+                   NeutralButtonName: String,
+                   onPositiveButtonToastMsg: String,
+                   onNegativeButtonToastMsg: String,
+                   onNeutralButtonToastMsg: String) : MaterialAlertDialogBuilder {
+        val dialog =  MaterialAlertDialogBuilder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(positiveButtonName) { p0, p1 ->
+                Toast.makeText(context, onPositiveButtonToastMsg, Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
+            .setNegativeButton(NegativeButtonName) { p0, p1 ->
+                Toast.makeText(context, onNegativeButtonToastMsg, Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
+            .setNeutralButton(NeutralButtonName) { p0, p1 ->
+                Toast.makeText(context, onNeutralButtonToastMsg, Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
         dialog.show()
 
         return dialog
     }
+
+
+    fun mExitDialogBoxWithCustomLayout(
+        context: Context,
+        layoutResourceId: Int,
+        yesButtonId: Int,
+        noButtonId: Int,
+    ): AlertDialog {
+        val mDialogView = View.inflate(context,layoutResourceId,null)
+        val exit = mDialogView.findViewById<Button>(yesButtonId)
+        val no = mDialogView.findViewById<Button>(noButtonId)
+        val mAlertDialog = AlertDialog.Builder(context)
+            .setView(mDialogView).show()
+        mAlertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        // action perform on buttons
+        exit.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context, "Exit", Toast.LENGTH_SHORT).show()
+        })
+
+        no.setOnClickListener(View.OnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(context, "Ok Enjoy", Toast.LENGTH_SHORT).show()
+        })
+
+        return mAlertDialog
+    }
+
+
 }
