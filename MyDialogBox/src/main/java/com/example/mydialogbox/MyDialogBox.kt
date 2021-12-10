31c724648651dcc55bcca1f52862b29fad94b03e
faraznb <alifarazzain@gmail.com>
2021-12-10 12:33:02 +0500

    Dialog Added

--- MyDialogBox/src/main/java/com/example/mydialogbox/MyDialogBox.kt
+++ MyDialogBox/src/main/java/com/example/mydialogbox/MyDialogBox.kt
@@ -1,11 +1,66 @@
 package com.example.mydialogbox
 
 import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 object MyDialogBox {
 
     fun toastMessage(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun mDialogBox(context: Context,
+                   title: String,
+                   message: String,
+                   positiveButtonName: String,
+                    NegativeButtonName: String,
+                    NeutralButtonName: String) {
+       MaterialAlertDialogBuilder(context)
+            .setTitle("Alert !")
+            .setMessage("Are You Really Want to Exit ?")
+            .setPositiveButton("Yes"
+            ) { p0, p1 ->
+                Toast.makeText(context, "You Click Yes Button", Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
+           .setNegativeButton("No") {
+                   p0, p1 -> Toast.makeText(context, "Ok Enjoy", Toast.LENGTH_SHORT).show()
+               p0.dismiss()
+           }
+           .setNeutralButton("Later") {
+                   p0, p1 -> Toast.makeText(context, "Ok", Toast.LENGTH_SHORT).show()
+               p0.dismiss()
+           }
+    }
+
+
+
+    fun mDialogBox(context: Context,
+                   title: String,
+                   message: String,
+                   positiveButtonName: String,
+                   NegativeButtonName: String) {
+        MaterialAlertDialogBuilder(context)
+            .setTitle("Alert !")
+            .setMessage("Are You Really Want to Exit ?")
+            .setPositiveButton("Yes"
+            ) { p0, p1 ->
+                Toast.makeText(context, "You Click Yes Button", Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
+            .setNegativeButton("No") {
+                    p0, p1 -> Toast.makeText(context, "Ok Enjoy", Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
+            .setNeutralButton("Later") {
+                    p0, p1 -> Toast.makeText(context, "Ok", Toast.LENGTH_SHORT).show()
+                p0.dismiss()
+            }
     }
 }
