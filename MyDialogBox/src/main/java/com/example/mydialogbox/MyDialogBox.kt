df5106a090d8dfd20d168366eb939b64912fc451
faraznb <alifarazzain@gmail.com>
2021-12-14 11:09:38 +0500

    Custom Layout Dialog completed

--- MyDialogBox/src/main/java/com/example/mydialogbox/MyDialogBox.kt
+++ MyDialogBox/src/main/java/com/example/mydialogbox/MyDialogBox.kt
@@ -1,8 +1,11 @@
 package com.example.mydialogbox
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -119,6 +122,120 @@ class MyDialogBox(ap : AppCompatActivity) {
 
         return mAlertDialog
     }
+
+
+    ///////////
+
+
+    fun mExitDialogBoxWithCustomLayout(
+        context: Context,
+        layoutResourceId: Int,
+        yesButtonId: Int,
+        noButtonId: Int,
+        onExitButtonToastMsg: String,
+        onNoButtonToastMsg: String
+    ): AlertDialog {
+        val mDialogView = View.inflate(context,layoutResourceId,null)
+        val exit = mDialogView.findViewById<Button>(yesButtonId)
+        val no = mDialogView.findViewById<Button>(noButtonId)
+        val mAlertDialog = AlertDialog.Builder(context)
+            .setView(mDialogView).show()
+//        mAlertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        // action perform on buttons
+        exit.setOnClickListener(View.OnClickListener {
+            mAppContex.finish()
+            Toast.makeText(context, onExitButtonToastMsg, Toast.LENGTH_SHORT).show()
+            mAlertDialog.dismiss()
+        })
+
+        no.setOnClickListener(View.OnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(context, onNoButtonToastMsg, Toast.LENGTH_SHORT).show()
+        })
+
+        return mAlertDialog
+    }
+
+    //////
+
+
+    fun mExitDialogBoxWithCustomLayout(
+        context: Context,
+        title: String,
+        message: String,
+        onExitButtonToastMsg: String,
+        onNoButtonToastMsg: String
+    ): AlertDialog {
+        val mDialogView = View.inflate(context,R.layout.mdialog_box_design,null)
+        val yes = mDialogView.findViewById<Button>(R.id.btn_yes)
+        val no = mDialogView.findViewById<Button>(R.id.btn_no)
+        val mtitle = mDialogView.findViewById<TextView>(R.id.tv_title)
+        val mmessage = mDialogView.findViewById<TextView>(R.id.tv_message)
+        mtitle.text = title
+        mmessage.text = message
+        val mAlertDialog = AlertDialog.Builder(context)
+            .setView(mDialogView).show()
+        mAlertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        // action perform on buttons
+        yes.setOnClickListener(View.OnClickListener {
+            mAppContex.finish()
+            Toast.makeText(context, onExitButtonToastMsg, Toast.LENGTH_SHORT).show()
+            mAlertDialog.dismiss()
+        })
+
+        no.setOnClickListener(View.OnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(context, onNoButtonToastMsg, Toast.LENGTH_SHORT).show()
+        })
+
+        return mAlertDialog
+    }
+
+    fun mExitDialogBoxWithCustomLayout(
+        context: Context,
+        title: String,
+        message: String,
+        onExitButtonToastMsg: String,
+        onNoButtonToastMsg: String,
+        backgroundColor: Int,
+        buttonsBackgroundColor: Int
+    ): AlertDialog {
+        val mDialogView = View.inflate(context,R.layout.mdialog_box_design,null)
+        mDialogView.setBackgroundColor(backgroundColor)
+        val yes = mDialogView.findViewById<Button>(R.id.btn_yes)
+        val no = mDialogView.findViewById<Button>(R.id.btn_no)
+        yes.setBackgroundColor(buttonsBackgroundColor)
+        no.setBackgroundColor(buttonsBackgroundColor)
+        val mtitle = mDialogView.findViewById<TextView>(R.id.tv_title)
+        val mmessage = mDialogView.findViewById<TextView>(R.id.tv_message)
+        mtitle.text = title
+        mmessage.text = message
+        val mAlertDialog = AlertDialog.Builder(context)
+            .setView(mDialogView).show()
+        mAlertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        // action perform on buttons
+        yes.setOnClickListener(View.OnClickListener {
+            mAppContex.finish()
+            Toast.makeText(context, onExitButtonToastMsg, Toast.LENGTH_SHORT).show()
+            mAlertDialog.dismiss()
+        })
+
+        no.setOnClickListener(View.OnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(context, onNoButtonToastMsg, Toast.LENGTH_SHORT).show()
+        })
+
+        return mAlertDialog
+    }
+
+
+    // this function is accessing mdialog_box_design in your project.
+//    fun libraryUi(
+//        context: Context
+//    ): View {
+//       val mDialogView = View.inflate(context,R.layout.mdialog_box_design,null)
+//        return mDialogView
+//    }
 
 
 }
--- app/src/main/java/com/example/dialogboxlibrary/MainActivity.kt
+++ app/src/main/java/com/example/dialogboxlibrary/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.example.dialogboxlibrary
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.Toast
 import com.example.mydialogbox.MyDialogBox
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setContentView(myDialogBox.libraryUi(this))
+//        frameLayout = findViewById(R.id.fram)
+        //val myDialogBox = MyDialogBox(this)
+        //myDialogBox.mExitDialogBoxWithCustomLayout(this,R.layout.dialog_box_design, R.id.btn_yes,R.id.btn_no)
+//        myDialogBox.mExitDialogBoxWithCustomLayout(this,"Hello Title","Hello dialog message","Exit", "Ok Enjoy",
+//            Color.CYAN,Color.GREEN)
+        //myDialogBox.libraryUi(this)
 
-        val myDialogBox = MyDialogBox(this)
-        myDialogBox.mExitDialogBoxWithCustomLayout(this,R.layout.dialog_box_design, R.id.btn_yes,R.id.btn_no)
+
     }
 
 
