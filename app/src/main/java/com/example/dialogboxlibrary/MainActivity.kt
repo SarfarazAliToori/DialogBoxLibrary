1932e3a47ba7bd709cb9935f483a43551a8a2baa
faraznb <alifarazzain@gmail.com>
2021-12-14 09:33:16 +0500

    Custom Layout Dialog in progress...

--- app/src/main/java/com/example/dialogboxlibrary/MainActivity.kt
+++ app/src/main/java/com/example/dialogboxlibrary/MainActivity.kt
@@ -10,6 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDialogBox = MyDialogBox(this)
+        myDialogBox.mExitDialogBoxWithCustomLayout(this,R.layout.dialog_box_design, R.id.btn_yes,R.id.btn_no)
     }
 
 
